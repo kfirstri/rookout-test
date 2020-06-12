dd3ab982794666c5d94db58400e5f7f75cba46c1
kfirstri <kfirstar@gmail.com>
2020-06-12 15:34:03 +0300

    blat

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const rookout = require('rookout');
 
 rookout.start({ token: '5cb047e401b00318adfa2bb2bd5fed752eba7239e3a77c0c51227e0b2a2369f2' });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+
+    let kfir = 100;
+
+    for (let i = 0 ; i < kfir; i ++) {
+        console.log(i);
+    }
+
+    res.send('Hello World!');
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
